4ba0d945cd12e9649a122ff2139c83fa3cf1bd8c
Winston <winston.tn@outlook.com>
2024-08-31 21:29:51 +0700

    minor eslint fix

--- src/components/About/Biography.jsx
+++ src/components/About/Biography.jsx
@@ -10,8 +10,9 @@ const Biography = () => {
         Hello, I&apos;m Tân Nguyễn; you may refer to me as Winston. I&apos;m a
         frontend&nbsp;developer dedicated to crafting visually&nbsp;appealing
         and highly&nbsp;functional digital experiences. With each project, I
-        consistently seek out innovative approaches to transform my clients'
-        visions into reality with a commitment to design excellence.
+        consistently seek out innovative approaches to transform my
+        clients&apos; visions into reality with a commitment to design
+        excellence.
       </p>
       <p>
         Every pixel on the screen, to me, is a story written in code and shaped
